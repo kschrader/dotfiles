86f60ca0a1ce1324465ce83588d0b72aa10ad6c6
Kurt Schrader <kurt@intentmedia.com>
2011-08-07 20:42:17 -0400

    One day I'll get my dotfiles under control. Today is not that day.

---
+++ alias.sh
@@ -0,0 +1,12 @@
+# Random
+alias jr="PATH=/Users/kschrader/code/intent/code/conf/vms/ruby/jruby/bin:$PATH"
+
+# GIT
+alias branch="git branch"
+alias master="git checkout master"
+alias checkout="git checkout"
+alias gs="git status"
+
+# bash
+alias ..="cd .."
+alias ls="ls --color"
